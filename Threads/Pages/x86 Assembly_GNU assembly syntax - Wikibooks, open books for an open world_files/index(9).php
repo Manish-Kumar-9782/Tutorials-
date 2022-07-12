9c789e823dbcb693422a5b8f93b3d4bdfe3c1b07
Manish Kumar <manishkumar9782@gmail.com>
2022-07-12 20:32:48 +0530

    removed duplicate again

--- Threads/Pages/x86 Assembly_GNU assembly syntax - Wikibooks, open books for an open world_files/index(9).php
+++
@@ -1,5 +0,0 @@
-// Force short review box to be on its own line.
-
-$(document).ready( function($) {
-  $('.flaggedrevs_short').wrap('<div style="display:block; clear:both; height:20px; line-height:18px; margin:5px;" />');
-});
