f3ee10d775addeededd7609f48097945e988def1
Manish Kumar <manishkumar9782@gmail.com>
2022-10-01 17:40:19 +0530

    worked on the showdatabase in Libmanagement

--- Class 2/LibManagement.js
+++ Class 2/LibManagement.js
@@ -1,4 +1,13 @@
 
+const addbook_button = document.getElementById("addbook")
+const showbooks_button = document.getElementById("showbooks")
+const updatebook_button = document.getElementById("updatebook")
+const deletebook_button = document.getElementById("deletebook")
+
+
+const addbook_container = document.getElementById("LibManagement");
+const showbooks_container = document.getElementById("showdatabase");
+
 class Book {
 
     static BookDataBase = [];
@@ -20,8 +29,87 @@ class Book {
 
     }
 
+    static getBookKeys() {
+        return ["primarykey", "title", "author", "subject", "pages", "price"]
+    }
+
 };
 
+new Book("JavaScript", "Suraj420", "How not to learn JavaScript", 1203, 40);
+new Book("HTML", "Gotam", "Fast way to learn HTML", 200, 150);
+new Book("Anything", "Narandera", "How to learn anything", 500, 120);
+
+
+class Database {
+
+    static showdatabase_thead = null;
+    static showdatabase_tbody = null;
+
+
+
+    static load_head() {
+
+        Database.showdatabase_thead = document.getElementById("showdatabase-thead")
+
+
+        const keys = Book.getBookKeys();
+
+        // first we need to make the table row for content of column headings.
+        const tr_element = document.createElement("tr");
+
+        for (let i = 0; i < keys.length; i++) {
+
+            // 1. we need to create th element for each key
+            let th_element = document.createElement("th")
+
+            // 2. put the content as the key string inside the th element
+            th_element.innerText = keys[i];
+
+            // 3. append each th element into tr_element.
+            tr_element.appendChild(th_element);
+
+        }
+
+        Database.showdatabase_thead.appendChild(tr_element);
+        return tr_element;
+    }
+
+    static load_body() {
+        Database.showdatabase_tbody = document.getElementById("showdatabase-tbody");
+
+        const books = Book.BookDataBase;
+
+        for (let i = 0; i < books.length; i++) {
+
+            let tr_element = document.createElement("tr");
+            Database.load_table_row(tr_element, books[i]);
+
+            Database.showdatabase_tbody.appendChild(tr_element);
+        }
+
+
+
+    }
+
+    static load_table_row(tr_element, book) {
+
+        let keys = Book.getBookKeys();
+
+        for (let i = 0; i < keys.length; i++) {
+
+            let th_element = document.createElement("th");
+
+            th_element.innerText = book[keys[i]];
+
+            tr_element.appendChild(th_element);
+        }
+
+    }
+
+}
+
+Database.load_head();
+Database.load_body();
 
 function getinputArray() {
     const form = document.forms[0];
@@ -43,3 +131,26 @@ function add_book() {
     new Book(...input_data)
 
 }
+
+function addBook() {
+    console.log("add book button is pressed");
+}
+
+function showBooks() {
+    console.log("show books button is pressed.");
+
+    // display:none; to hide.
+    // display:block: to show.
+
+    // element.style.display = value;
+
+    addbook_container.style.display = "none";
+    showbooks_container.style.display = 'block';
+
+
+}
+
+// binding the onclick event
+
+addbook_button.onclick = addBook;
+showbooks_button.onclick = showBooks;
