9c648eebda105bdda6e1f3c907ba4ea7aa939583
Manish Kumar <manishkumar9782@gmail.com>
2022-11-17 11:12:05 +0530

    worked on the more string function in class

--- php-Test/StringTest.php
+++ php-Test/StringTest.php
@@ -123,7 +123,7 @@
 
     $pos = strpos($str, "NoOne");
     heading(3, "Result of strpos function: ");
-    display('p', "<b>Output: </b> Position of NoOne: " . $pos, "data");
+    display('p', "<b>Output: </b> Position of 'NoOne': " . $pos, "data");
     put_sep();
     ?>
 
@@ -136,29 +136,93 @@
 
     $str = "    Jello, Hello,   ";
 
-    display('pre', $str, "data", "String: ");
+    display('pre', $str, "data data-bg", "String: ");
     // Now we will remove additional leading edge whitespace form the string.
     // using the trim method.
 
     $trimmed_str = trim($str);
     heading(3, "Result of trim operation: ");
-    display('pre', $trimmed_str, "data", "Output: ");
+    display('pre', $trimmed_str, "data data-bg", "Output: ");
     put_sep();
     ?>
 
     <!-- count chars -->
     <?php
-    heading(1, "Counting each charcter frequency from a string.");
+    heading(1, "Counting each character frequency from a string.");
     heading(3, "Our String");
     $str = "HelloMailAddress@mailEaxample.com";
     display('pre', $str, "data", "String: ");
 
 
     $result = count_chars($str, 3); // by default mode 0;
-    heading(3, "Output array: ");
+    heading(3, "Output: ");
     // show_array($result);
     display('pre', $result, "data", "All Unique Chars: ");
+    put_sep();
+    ?>
 
+    <!-- string reverse -->
+
+    <?php
+    heading(1, "Reverse a String.");
+    heading(3, "Our String");
+    $str = "HelloMailAddress@mailEaxample.com";
+    display('pre', $str, "data", "String: ");
+
+
+    $result = strrev($str);
+    heading(3, "Output: ");
+    // show_array($result);
+    display('pre', $result, "data", "Reversed String: ");
+    put_sep();
+    ?>
+
+    <?php
+    heading(1, "Replacing a String");
+    heading(3, "Our String");
+    $str = "HelloMailAddress@mailEaxample.com";
+    display('pre', $str, "data", "String: ");
+
+
+    $result = str_replace("Address", "Gotam", $str); //
+    heading(3, "Output: ");
+    // show_array($result);
+    display('pre', $result, "data", "Reversed String: ");
+
+
+    echo "<br><br>";
+
+    // a,e,i,o,u
+
+    $items = ['a', 'e', 'i', 'o', 'u'];
+    heading(2, "Replacing characters: ");
+    display('pre', $str, "data", "String: ");
+    heading(3, "Replace These character by .");
+
+    $replaced = str_replace($items, ".", $str);
+    display('pre', $replaced, "data", "String: ");
+
+    put_sep();
+
+    // Replacing a sequence of word with another sequence of words.
+
+
+    heading(2, "Replacing a sequence of word with another sequence of word.");
+    $str = "Gotam,21,5.7,B+";
+    $seq1 = ["Gotam", "21", "5.7", "B+"];
+    $seq2 = ["Narendra", "22", "5.6", "A+"];
+
+    display('pre', $str, "data", "Subject String: ");
+
+    heading(3, "Search Sequence:");
+    show_array_values($seq1);
+
+    heading(3, "Replace Sequence");
+    show_array_values($seq2);
+
+    heading(3, "Output: ");
+    $replaced = str_replace($seq1, $seq2, $str);
+    display('pre', $replaced, "data", "Replaced String: ");
 
     ?>
 </body>
