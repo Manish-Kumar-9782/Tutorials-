671128e8e14065570add7ece73befdc6ec1eb1b6
Manish Kumar <manishkumar9782@gmail.com>
2022-12-27 15:34:45 +0530

    Initiated a JavaScript class

---
+++ JavaScript Beginning/Introduction to Operators.js
@@ -0,0 +1,37 @@
+
+/**
+ * Operators in JavaScript
+ *
+ *      1. Arithmetic Operator
+ *      2. Assignment Operator
+ *      3. Increment or Decrement Operator
+ *      4. Relational Operator
+ *      5. Logical Operator
+ */
+
+/**
+ *  Arithmetic Operators
+ *      1. + : plus operator
+ *      2. - : minus operator
+ *      3. / : division operator
+ *      4. * : multiply operator
+ *      5. % : modulus operator
+ */
+
+// Working with Number , integer (whole number), floating point number (decimal number)
+
+let a = 10;
+let b = 20;
+
+// here '=' is called assignment operator
+// assignment operator is used to give right value to left variable.
+
+// Now we will perform some operation with console message.
+
+
+console.log("a :", a);
+console.log("b :", b);
+console.log("adding a & b:", a + b);
+console.log("subtracting a & b:", a - b);
+console.log("subtracting a * b:", a * b);
+console.log("subtracting a / b:", a / b);
---
+++ JavaScript Beginning/intro to javascript.js
@@ -0,0 +1,53 @@
+/**
+ * Multi comment section
+ * 
+ * Keywords: Keywords are the predefined word in javaScript, which are reserved for special purpose
+ *      in our javaScript programming, and these words are known as keywords.
+ *      e.g: let, const, var, 
+ * 
+ * Identifier: Identifiers are user-defined names of variables, function, class, object,..., etc. 
+ * 
+ * Variables: Variables are container for values or other kind of object. if our variable is defined with
+ *  var and let keyword then we can changer our variable value.
+ * 
+ *  if variable is defined with const keyword then we can't change variable value.
+ * 
+ * Since 'var' is ES5 (EcmaScript version 5) part and 'let' comes with ES6, 'let' is more secure than 'var'
+ * hence we will use only let keyword for declaring variables.
+ */
+
+//  Now make a variable with let 
+
+let letx = 10;
+
+/**
+ * Rule to naming variable:
+ * 
+ *      1. Variable name should not start form numbers/digits
+ *      2. Does not support special character in a variable.
+ *      3. A Variable Name should not have white-space in b/w two words.
+ *          eg. let my name : not allowed
+ *      4. A Variable Name can have underscore  "_" in b/w two or multiple words words.
+ *          eg. let my_name : allowed
+ *      5. A variable Name cannot be wrapped inside the double or single quotes.
+ *          eg. let "my_name" or 'my_name' : not allowed
+ */
+
+let my_name = 10;
+
+// To show some message on console screen we can use console interface.
+
+console.log("Hello world..!");
+
+/**
+ * Note: here console.log() is a function to show message on the browser console screen.
+ * 
+ * "Hello World..!" is a string which is wrapped inside the "" double quotes.
+ */
+
+let a = 10;
+let b = 20;
+
+console.log("value of a + b: ", a + b);
+console.log("value of a + b: ", b);
+console.log("value of a + b: ", a);
