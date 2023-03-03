2103745a677488aced184005fa4f73937492f8e0
Manish Kumar <manishkumar9782@gmail.com>
2023-03-03 15:07:22 +0530

    worked on the while loop

--- JavaScript Beginning/if-else.js
+++ JavaScript Beginning/if-else.js
@@ -74,26 +74,77 @@ switch(light){
         break;
 } */
 
-function StartPattern1() {
+function trafficLight(light) {
 
-    // first we will make variable to store the string in which we will create the pattern.
+    switch (light) {
+        case "green":
+            console.log("The light is green: you can go.");
+            break;
+        case "orange":
+            console.log("The light is orange: Be Careful accident ho jayega.");
+            break;
+        case "red":
+            console.log("The light is red: You can't go.");
+            break;
+        default:
+            console.log("Traffic system has been failed. call the traffic police.");
+            break;
+    }
+}
 
-    let pattern = "";
-    let lines = 5;
 
-    // Now we will outer loop for each row.
-    for (let row = 0; row < lines; row++) {
+// 
+function runLoop() {
 
-        // Now in each row we will put some starts.
-        for (let columns = 0; columns < row; columns++) {
-            pattern += "🐹";
+    while (true) {
 
+        let light = prompt("Enter traffic signal rgb color:")
+
+
+        if (light === "green" || light === "orange" || light === "red") {
+            trafficLight(light)
         }
-        pattern += "\n";
+        else {
+            // this break will close our while loop
+            break;
+        }
+    }
+}
+
+function pyramid(rows, p = "*") {
+
+    let row = 0; // to initialize row form 0 at the beginning of our outer loop.
+    let lines = rows; // how many number of lines of pattern we need.
+    let spaces = lines;
+    let pattern = "";  // in this we will store our pattern.
+
+    // initializing while loop and testing the condition.
+    while (row < lines) {
+
+        let stars = 2 * row + 1; // for each row we will decide that how many stars we need to print.
+        let columns = 0; // now we need to initialize our inner loop counter variable.
+
+        // initializing our inner loop and testing the condition
+        while (columns < (spaces + stars)) {
+
+            // if our column number is less than our spaces then we will print a white space. 
+            if (columns < spaces) {
+                pattern += " ";
+            }
+            else {
+                // if our column number is greater than our spaces then we will print "*".
+                pattern += p;
+            }
+            // increasing the column number after printing each character either " " or "*".
+            columns++;
+        }
+
+
+        // after print a line we need to do.
+        pattern += "\n"; // need to add a new line character in our pattern.
+        row++; // need to increase the row number.
+        spaces--; // need to decrease the number of spaces
     }
 
     console.log(pattern);
-
 }
-
-StartPattern1();
