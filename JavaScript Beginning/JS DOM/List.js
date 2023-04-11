6fcc1eb23ce25ebec48395b717c7b3a2504b8dcb
Manish Kumar <manishkumar9782@gmail.com>
2023-04-11 14:48:35 +0530

    worked on the list edit feature

--- JavaScript Beginning/JS DOM/List.js
+++ JavaScript Beginning/JS DOM/List.js
@@ -1,6 +1,10 @@
 // Create a list block or a container.
 
-// li 
+/*
+    1. create an element (any type)
+    2. attach (put) that element to the the document.
+    3. add attribute and content to the created element.
+*/
 
 class InputElement {
     constructor(parent, list) {
@@ -30,55 +34,70 @@ class InputElement {
 
 class ListElement {
 
-    constructor(parent, content) {
-        /**
-         * parent: it is an element which will contain the list item.
-         * 
-         * content: it is the content for our list item element.
-         */
+    constructor(parent, content, listContainer) {
         this.parent = parent;
         this.content = content;
-
-
+        this.listContainer = listContainer;
         // first creating the li element
-        this.liElement = document.createElement("li");
-        this.span = document.createElement("span");
-        this.button = document.createElement("button");
-        // now putting some text content inside that element.
-        this.span.innerText = this.content;
-        this.button.innerText = "Delete";
+        this.liElement = document.createElement("li"); // list item
+        this.spanContent = document.createElement("span"); // span for text
+        this.actions = document.createElement("div"); // actions a dive to contain 
+        // the buttons 
+        this.deleteButton = document.createElement("button"); // delete button
+        this.editButton = document.createElement("button"); // edit button
+
+        // now putting some text content inside that element.actions
+        this.spanContent.innerText = this.content;
+        this.deleteButton.innerText = "Delete";
+        this.editButton.innerText = "edit";
 
         this.liElement.classList.add("d-flex");
         this.liElement.classList.add("justify-content-between");
 
-        this.liElement.appendChild(this.span);
-        this.liElement.appendChild(this.button);
+        this.liElement.appendChild(this.spanContent);
+        this.liElement.appendChild(this.actions)
+
+        // inserting the delete and edit button inside the actions.
+        this.actions.appendChild(this.editButton);
+        this.actions.appendChild(this.deleteButton);
 
         // now append this element inside the parent.
         this.parent.appendChild(this.liElement);
 
 
-        this.button.addEventListener("click", (e) => {
-            this.liElement.remove();
-            delete this;
-        })
+        // an event listener to delete the element.
+        this.deleteButton.addEventListener("click", (e) => this.delete(e))
+
+        // adding a new event listener for edit button
+        this.editButton.addEventListener("click", (e) => this.edit(e));
     }
 
     setId(index) {
-        this.button.dataset.id = index;
+        this.deleteButton.dataset.id = index;
+    }
+
+    delete(e) {
+        this.liElement.remove();
+        delete this;
+    }
+
+    edit(e) {
+        let text = this.spanContent.innerText;
+        this.listContainer.input.value = text;
+        this.listContainer.updateRef = this.spanContent;
     }
 
 }
 
 // ul, ol
+// static LIST = []; // an array to hold all the list elements.
 class ListContainer {
-
-    // static LIST = []; // an array to hold all the list elements.
-
     constructor(parent, listType = 'ul') {
         this.listType = listType;
         this.parent = parent;
         this.items = [];
+        this.updateRef = null; // to hold the reference of the element for which we need to update the content
+
         // first we will create a new list element.
         this.listContainer = document.createElement(this.listType);
 
@@ -89,11 +108,31 @@ class ListContainer {
 
         this.input.addEventListener('keydown', (e) => {
             if (e.key === 'Enter') {
-                this.getText()
+
+                if (this.updateRef) {
+                    // if we have any element ref inside the this.updateRef variable.
+                    this.updateRef.innerText = this.input.value;
+                    this.input.value = '';
+                    this.updateRef = null;
+                } else {
+                    this.getText()
+                }
             }
         });
 
         this.parent.appendChild(this.listContainer);
+    }
+
+    addItem(content) {
+        let li = new ListElement(this.listContainer, content, this);
+        this.items.push(li);
+        li.setId(this.items.indexOf(li));
+    }
+
+    deleteItem(index) {
+        let element = this.items[index];
+        element.liElement.remove();// remove method on element 
+        this.items.splice(index, 1);
     }
 
     setClass(str) {
@@ -108,17 +147,5 @@ class ListContainer {
         this.addItem(this.input.value);
         this.input.value = ""; // resetting value of 
         // input element.
-    }
-
-    addItem(content) {
-        let li = new ListElement(this.listContainer, content);
-        this.items.push(li);
-        li.setId(this.items.indexOf(li));
-    }
-
-    deleteItem(index) {
-        let element = this.items[index];
-        element.liElement.remove();// remove method on element 
-        this.items.splice(index, 1);
     }
 }
