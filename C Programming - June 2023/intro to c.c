b672b7f963020cfa38927a119acd3f418c956de0
Manish Kumar <manishkumar9782@gmail.com>
2023-06-09 17:20:00 +0530

    basic c intro

---
+++ C Programming - June 2023/intro to c.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+// this is a single line comment
+
+
+/* this is a comment (multi line comment)
+	Operators in C
+		1. Arithmetic Operators
+			-> + : plus operator
+			-> - : minus operator
+			-> / : divide operator
+			-> * : multiply operator
+			-> % : modulus opertor
+			
+		2. Assignment Operators
+		3. Increment and decrement
+		4. Relational/Comparison Operator
+		5. Logical operator
+		6. ternary operator
+		7. bitwise operator.
+		
+		\n: newline charater
+		' ' : single quote only for single character
+		" ": double quote only for string
+		; : semi-colon , to terminate the single statement
+		: : colon
+		() : round-bracket / paranthesis
+*/
+
+void main(){
+	
+	int a=0, b=0; // declaration and intialization
+	int c; // not initalized.
+	
+	printf("Value of a: %d\n", a);
+	printf("Value of b: %d\n", b);
+	printf("Value of c: %d\n", c);
+	
+}
