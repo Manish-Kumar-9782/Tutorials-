94a37f6a383c89b8fe05680b574dd2e6059403d7
Manish Kumar <manishkumar9782@gmail.com>
2023-06-21 17:39:02 +0530

    worked on the logical and relational operator

--- C Programming - June 2023/Operators Introduction.c
+++ C Programming - June 2023/Operators Introduction.c
@@ -9,7 +9,12 @@
 			=, +=, -=, /=, *=, %=
 
 		3. Comparison Operator
-			==, <=, >=, <, >
+			== (equality Operator), 
+			!= (Not Equal Operator), 
+			<= (less than or equal to), 
+			>= (greater than or equal to), 
+			<  (less than operator), 
+			>  (greater than operator)
 
 		4. Logical Operators
 			&&, ||, !
@@ -30,6 +35,7 @@
 			implicit type casting --> lower data type to higher data type (primitive data type)
 			explicit type casting --> 
 
+		bool: true (1), false (0)	
 	*/
 
 void main() {
@@ -42,7 +48,8 @@ void main() {
 
 	int a = 30; // initialization of variable a
 	int b = 50;
-
+	int c = 20;
+	int d = 30;
 
 	printf("==================== Arithmetic Operator ====================\n");
 
@@ -79,4 +86,62 @@ void main() {
 
 	printf("\n\na: %d, b: %d\n", a, b);
 	printf("=============================================================\n");
+	
+
+
+	printf("\n\n==================== Relational/Comparison Operator====================\n");
+	c += 10;
+	printf("a: %d, b: %d, c: %d, d: %d\n", a, b, c, d);
+
+	printf("is a > b: %d \n", a > b);
+	printf("is a < b: %d \n", a < b);
+
+	// comparing that c is greater than d or equal to d
+	printf("is c > d or c == d: %d\n", c >= d);
+	printf("is a > d or a == d: %d\n", a >= d);
+
+	// comparing that a or b equal 
+	printf("is a == b: %d\n", a == b);
+
+	// comparing that a is not equal to b;
+	printf("is a not equal to b: %d\n", a != b);
+	printf("\n\n=============================================================\n");
+
+	printf("\n\n==================== Logical Operator ====================\n");
+	// && and || are binary operators
+	// ! is a unary operator
+
+	// <expression> && <expression> --> boolean result
+	// <expression> || <expression> --> boolean result
+	// !<expression> --> boolean result
+
+	/*
+		&& AND Operator
+
+		true && true --> true
+		true && false --> false
+		false && true --> false
+		false && false --> false
+
+		|| OR OPerator
+
+		true || true --> true
+		true || false --> true
+		false || true --> true
+		false || false --> false
+
+		! NOT Operator
+
+		!true ---> false
+		!false ---> true
+	*/
+	printf("a: %d, b: %d, c: %d, d: %d\n", a, b, c, d);
+
+	printf("is b greater than a AND less than c: ");
+	printf("%d && %d ---> %d\n", b > a, b<c, b>a && b < c);
+
+	printf("is c greater than a AND less than b: ");
+	printf("%d && %d ---> %d\n", c > a, c < b, c>a&& c < b);
+
+	printf("\n\n=============================================================\n");
 }
