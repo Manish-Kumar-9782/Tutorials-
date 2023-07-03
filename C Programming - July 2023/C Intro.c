ff60efd6553cd474285e2c34b9236086fec6f1fe
Manish Kumar <manishkumar9782@gmail.com>
2023-07-03 18:02:49 +0530

    worked on the intro to c

---
+++ C Programming - July 2023/C Intro.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/* Now our entry point is main function.
+ () : round barcket or paranthesis
+ {}: curly bracket
+ [] : square bracket
+statement: it is a code continuous of single line or multi line terminated
+ by semicolon.
+ 
+ string: it is a (text message) a sequence of characters enclosed by 
+ 	double quotes
+*/
+
+void main(){
+	printf("Hello world");
+}
