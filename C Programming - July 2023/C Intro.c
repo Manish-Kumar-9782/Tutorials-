4ecf01885a6a1525d0db23d2ab3b35454ba53010
Manish Kumar <manishkumar9782@gmail.com>
2023-07-04 18:07:25 +0530

    worked on the data-type, indentifier & keyword, variable

--- C Programming - July 2023/C Intro.c
+++ C Programming - July 2023/C Intro.c
@@ -9,8 +9,45 @@ statement: it is a code continuous of single line or multi line terminated
  
  string: it is a (text message) a sequence of characters enclosed by 
  	double quotes
+ 	
+ 	Indentifier & Keyword: 
+ 	
+ 	keyword: it is predefined word which is reserved for doing special kind 
+ 	operation.
+ 	keywords: int, float, void, char, .. etc.
+ 	
+ 	void: keyword for data-type, denote no type.
+ 	
+ 	variable:  it is holder which can hold values inside it.
+ 	Note: a variable can only store/hold the single type of value
+ 	which is defined during declaration of variable.
+ 	
+ 	declaration of variable;
+ 	
+ 	data-type variable;
+ 	
+ 	our main data type: int, float, char, double, void.
+ 	modifers: short, long
+ 	
+ 	for number: int, float, double.
+ 	character: char.
+ 	
+ 	format specifier: it is used to specify that what kind of value
+ 	we are going to print.
+ 	
+ 	int: %d, float: %f, char: %c, double: %lf
+ 	
+ 	\n: newline character, it is used to create new line inside the string.
 */
 
 void main(){
-	printf("Hello world");
+	// here 'main' & 'printf' are both identifier
+	printf("Hello world \n");
+	
+	int Integer_number; // declaring a variable named as Integer_variable
+	
+	// assingning a value to Integer_number variable.
+	Integer_number = 1335;
+	
+	printf("%d",Integer_number);
 }
