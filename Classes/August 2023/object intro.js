211e914842abcd37b5f2e5c02c2ea667e4405541
Manish Kumar <manishkumar9782@gmail.com>
2023-10-03 09:41:30 +0530

    worked on the object introduction

---
+++ Classes/August 2023/object intro.js
@@ -0,0 +1,27 @@
+
+/**
+ * object:
+ * 
+ *  let Object_name = {
+ *      key: value,
+ *      "key": value
+ * }
+ * 
+ */
+
+let Student = {
+    name: 'pooja',
+    age: 21,
+    "height": 5.5,
+    bloodGroup: "AB+",
+}
+
+let Student2 = {
+    name: 'kunal',
+    age: 22,
+    "height": 5.7,
+    bloodGroup: "B+",
+}
+
+
+
