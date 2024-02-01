a527960483ad9b2e3088acafd55364fbd340cf94
Manish Kumar <manishkumar9782@gmail.com>
2024-02-01 12:29:45 +0530

    working dom element attributyes, properties and method.

--- Classes/Nov 2023/dom_intro.js
+++ Classes/Nov 2023/dom_intro.js
@@ -6,6 +6,7 @@ let h1_s = document.getElementsByTagName("h1")
 // this will get the only element having the id
 // js-task.
 let ul_list = document.getElementById("js-task")
+let ol_list = document.getElementById("ol-list")
 
 // this will get all the list item present in the html
 let all_list_items = document.getElementsByTagName("li")
@@ -15,3 +16,27 @@ let all_list_items = document.getElementsByTagName("li")
 // list.
 
 let js_task_li_items = ul_list.getElementsByTagName("li")
+
+
+
+// highlighting the JavaScript word from list.
+
+// const word = prompt("Enter word: ")
+// const replaceBy = prompt("new word: ")
+
+// for (let i = 0; i < js_task_li_items.length; i++) {
+//     let li_item = js_task_li_items[i]
+//     let li_item_content = li_item.innerHTML
+
+//     li_item.innerHTML = li_item_content.replaceAll(
+//         word, replaceBy)
+// }
+
+
+function removeItem(parentNode) {
+
+    let childNodeIndex = prompt("child node index: ")
+
+    parentNode.removeChild(parentNode.children[childNodeIndex])
+
+}
