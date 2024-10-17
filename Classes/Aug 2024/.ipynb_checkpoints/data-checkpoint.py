a36ed8de58c641863cbd52ae0b10730fa3ef05fe
Manish Kumar <manishkumar9782@gmail.com>
2024-10-17 18:04:19 +0530

    worked on classes

---
+++ Classes/Aug 2024/.ipynb_checkpoints/data-checkpoint.py
@@ -0,0 +1,25 @@
+people_list = [
+    {"name": "Alice", "age": 30, "height": 165, "bloodGroup": "A+"},
+    {"name": "Bob", "age": 25, "height": 180, "bloodGroup": "B-"},
+    {"name": "Charlie", "age": 22, "height": 175, "bloodGroup": "O+"},
+    {"name": "David", "age": 28, "height": 170, "bloodGroup": "AB+"},
+    {"name": "Eva", "age": 35, "height": 160, "bloodGroup": "O-"},
+    {"name": "Frank", "age": 40, "height": 185, "bloodGroup": "A-"},
+    {"name": "Grace", "age": 21, "height": 168, "bloodGroup": "B+"},
+    {"name": "Henry", "age": 32, "height": 175, "bloodGroup": "O+"},
+    {"name": "Isabel", "age": 27, "height": 172, "bloodGroup": "AB-"},
+    {"name": "Jack", "age": 24, "height": 178, "bloodGroup": "A-"},
+]
+
+people_list2 = [
+    {"name": "Alice", "age": 30, "height": 165, "bloodGroup": "A+"},
+    {"name": "Bob", "age": 25, "height": 180, "bloodGroup": "B-"},
+    {"name": "Charlie", "age": 22, "height": 175, "bloodGroup": "O+"},
+    {"name": "David", "age": 28, "height": 170, "bloodGroup": "AB+"},
+    {"name": "Eva", "age": 35, "height": 160, "bloodGroup": "O-"},
+    {"name": "Frank", "age": 40, "height": 185, "bloodGroup": "A-"},
+    {"name": "Grace", "age": 21, "height": 168, "bloodGroup": "B+"},
+    {"name": "Henry", "age": 32, "height": 175, "bloodGroup": "O+"},
+    {"name": "Isabel", "age": 27, "height": 172, "bloodGroup": "AB-"},
+    {"name": "Jack", "age": 24, "height": 178, "bloodGroup": "A-"},
+]
